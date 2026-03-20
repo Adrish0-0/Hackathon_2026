187e22eca8a5ba16a3c5b44f763574fcf38aea26
Adrish0-0 <adrianchan0603@gmail.com>
2026-03-20 10:30:11 +0800

    Initialize application with logging and bot setup

---
+++ application.py
@@ -0,0 +1,24 @@
+import sys
+import logging
+from datetime import datetime
+from basis import *
+from strats import *
+
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s - %(levelname)s - %(message)s",
+    handlers = [
+        logging.FileHandler(f'bot_{datetime.now().strftime("%Y%m%d")}.log'),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+
+if __name__ == "__main__":
+    bot = RSITradingBot(
+        coin = "BTC",
+        trade_quantity = 0.001,
+    )
+    
+    bot.run()
